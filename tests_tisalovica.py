4e353d2ad3a030a64442d1a48e71deac96237585
NikaTisalovica <144417066+NikaTisalovica@users.noreply.github.com>
2023-09-08 08:51:02 +0300

    Add files via upload

---
+++ tests_tisalovica.py
@@ -0,0 +1,19 @@
+#30 dienas Python programmēšanas valodā
+vards = 'Nika'
+uzvards = 'Tisalovica'
+vards_uzvards = vards+uzvards
+valsts = 'Latvija'
+pilseta = 'Sigulda'
+gadi = 16
+gads = 2022
+skola,klase= 'svg', 311
+print(vards, uzvards, vards_uzvards, valsts, pilseta, gadi, gads, skola, klase)
+print(type(vards))
+print(type(uzvards))
+print(type(vards_uzvards))
+print(type(valsts))
+print(type(pilseta))
+print(type(gadi))
+print(type(gads))
+print(type(skola))
+print(type(klase))
